ab65881fa8b3196ded115714eb53385f91b1f7e3
Ashley Georgeson <ashleygeorgeson@me.com>
2021-01-31 14:05:50 +0000

    renamed function return variable

--- 02_loose_change.py
+++ 02_loose_change.py
@@ -14,7 +14,7 @@ import sys
 
 def loose_change(cents):
     '''Takes an amount of currency in #cents, returns dictionary of least coins'''
-    min_coins = {
+    change_dict = {
         'Nickels' : '',
         'Pennies' : '',
         'Dimes' : '',
@@ -26,21 +26,21 @@ def loose_change(cents):
     elif type(cents) == float:
         cents = int(cents)
 
-    # Calculate number of quarters first (as largest), remove from pennies and add to min_coins
+    # Calculate number of quarters first (as largest), remove from pennies and add to change_dict
     quarters = cents // 25
     cents = cents - (quarters * 25)
-    min_coins['Quarters'] = quarters
-    # Calculate number of dimes next (as next largest), remove from pennies and add to min_coins
+    change_dict['Quarters'] = quarters
+    # Calculate number of dimes next (as next largest), remove from pennies and add to change_dict
     dimes = cents // 10
     cents = cents - (dimes * 10)
-    min_coins['Dimes'] = dimes
-    # Calculate number of nickels next (as next largest), remove from pennies and add to min_coins
+    change_dict['Dimes'] = dimes
+    # Calculate number of nickels next (as next largest), remove from pennies and add to change_dict
     nickels = cents // 5
     cents = cents - (nickels * 5)
-    min_coins['Nickels'] = nickels
-    # Then finally add the remaining #cents to min_coins as 'Pennies'
-    min_coins['Pennies'] = cents
-    return(min_coins)
+    change_dict['Nickels'] = nickels
+    # Then finally add the remaining #cents to change_dict as 'Pennies'
+    change_dict['Pennies'] = cents
+    return(change_dict)
 
 def main():
     # Test Case 1 - expected {'Nickels': 0, 'Pennies': 4, 'Dimes': 0, 'Quarters': 1}"
@@ -53,8 +53,8 @@ def main():
     #cents = -2
     # Test Case 5 - expected {'Nickels': 0, 'Pennies': 3, 'Dimes': 0, 'Quarters': 0}" FAIL!!!
     cents = 3.9
-    min_coins = loose_change(cents)
-    print(min_coins)
+    change_dict = loose_change(cents)
+    print(change_dict)
 
 # Check to see if this file is the "__main__" script being executed
 if __name__ == '__main__':
