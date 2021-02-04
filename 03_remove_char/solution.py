1b0a390f63101e3326a0ccb25c6dd6cf4e5dbff8
Ashley Georgeson <ashleygeorgeson@me.com>
2021-02-04 08:35:34 +0000

    simplify main function

--- 03_remove_char/solution.py
+++ 03_remove_char/solution.py
@@ -14,26 +14,7 @@ def remove_char(s):
     return(s[1:len(s)-1])
 
 def main():
-    string = '123456789'
-    # Test Case 1 Values - "a was [1,2], b was [1], expected [2]"
-    #a = [1,2]
-    #b = [1] 
-    # Test Case 2 Values - "a was [1,2,2], b was [1], expected [2,2]"
-    #a = [1,2,2]
-    #b = [1]
-    # Test Case 3 Values - "a was [1,2,2], b was [2], expected [1]"
-    #a = [1,2,2]
-    #b = [2] 
-    # Test Case 4 Values - "a was [1,2,2], b was [], expected [1,2,2]"
-    #a = [1,2,2]
-    #b = [] 
-    # Test Case 5 Values - "a was [], b was [1,2], expected []"
-    #a = []
-    #b = [1,2] 
-    print("INITAL STRING:" , string)
-    string = remove_char(string)
-    print("FINAL STRING:" , string)
-
+    print("FINAL STRING:" , remove_char('123456789'))
 
 # Check to see if this file is the "__main__" script being executed
 if __name__ == '__main__':
