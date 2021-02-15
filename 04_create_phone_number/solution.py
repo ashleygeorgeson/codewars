03d157a7839954c5d20a36c94dfc81c47231e6d2
ashleygeorgeson <ashleygeorgeson@me.com>
2021-02-15 20:54:39 +0000

    04_create_phone_number simplified

--- 04_create_phone_number/solution.py
+++ 04_create_phone_number/solution.py
@@ -14,9 +14,7 @@ START_MESSAGE = "Create Phone Number Script"
 location = os.path.abspath(__file__)
 
 def create_phone_number(n):
-    num = ''
-    for i in n: num += str(i)
-    return('(' + num[0:3] + ') ' + num[3:6] + '-' + num[6:11])
+    return("({}{}{}) {}{}{}-{}{}{}{}".format(*n))
 
 # Module Functions and Classes
 def main(*args):
@@ -24,13 +22,9 @@ def main(*args):
 
     Displays the full path to this script, and a list of the arguments passed to the script.
     """
-    #print(START_MESSAGE)
-    #print("Script Location:", location)
+    print(START_MESSAGE)
+    print("Script Location:", location)
     print("Arguments Passed:", *args)
-    #print("Arguments Passed:", *args[0])
-    #print("Arguments Passed:", *args[1])
-    #print(type(*args))
-    #print(type(tuple(*args.data)))
     n = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0]
     phone_number = create_phone_number(*args, n=n)
     print(phone_number)
@@ -38,14 +32,4 @@ def main(*args):
 # Check to see if this file is the "__main__" script being executed
 if __name__ == '__main__':
     _, *script_args = sys.argv
-    #print(*script_args)
-    parser = argparse.ArgumentParser()
-    parser.add_argument('--data', nargs='+', type=int)
-    args = parser.parse_args()
-    my_list = list(args.data)
-    print(my_list)
-    #print(sys.argv)
-    #print(sys.argv[0])
-    #print(sys.argv[1])
-    #main(*script_args)
-    main(my_list)
+    main(*script_args)
