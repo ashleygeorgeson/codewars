2c69bd02ec4b1fbd7c110761d88e5ad45762dd15
ashleygeorgeson <ashleygeorgeson@me.com>
2021-02-16 10:02:15 +0000

    integrated argparser into 04

--- 04_create_phone_number/solution.py
+++ 04_create_phone_number/solution.py
@@ -12,6 +12,9 @@ START_MESSAGE = "Create Phone Number Script"
 
 # Module "Global" Variables
 location = os.path.abspath(__file__)
+parser = argparse.ArgumentParser(description='Create Phone Number Script')
+parser.add_argument('-l','--list', nargs='+', help='<Required> Set flag', type=int,
+    default=[1,2,3,4,5,6,7,8,9,0],required=False)
 
 def create_phone_number(n):
     return("({}{}{}) {}{}{}-{}{}{}{}".format(*n))
@@ -24,12 +27,15 @@ def main(*args):
     """
     print(START_MESSAGE)
     print("Script Location:", location)
+    print("Running '{}'".format(__file__))
     print("Arguments Passed:", *args)
     n = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0]
-    phone_number = create_phone_number(*args, n=n)
+    #phone_number = create_phone_number(*args, n=n)
+    phone_number = create_phone_number(*args)
     print(phone_number)
 
 # Check to see if this file is the "__main__" script being executed
 if __name__ == '__main__':
-    _, *script_args = sys.argv
-    main(*script_args)
+    args = parser.parse_args()
+    #_, *script_args = sys.argv
+    main(args.list)
