7618fda0e1020fdc0856e8ea9635edfa858e7f42
ashleygeorgeson <ashleygeorgeson@me.com>
2021-02-28 17:56:25 +0000

    3x functions for decrypt! no error checking

--- 08-simple-encryption-alternating-split/solution.py
+++ 08-simple-encryption-alternating-split/solution.py
@@ -20,9 +20,68 @@ location = os.path.abspath(__file__)
 
 def decrypt(encrypted_text, n):
     """decrypt function
+    For building the decrypted string:
+    Opposite of the encrypt function
+    Split the string in two
+    find len of string, floor div = second half, floor div + mod = 1st half
+    Take every 2nd char from the string, then the other chars, that are not every 2nd char, and concat them as new String.
+    Do this n times!
+
+
+    split original string into two halves, str1 and str2
+    loop through the length of the longest string, and append str2,str1 char into a new string
+
 
     """
-    print("Decrypt:", args.decrypt, args.text, args.n)
+    #first_half_chars = len(encrypted_text)//2 + len(encrypted_text)%2 -1
+    #second_half_chars = len(encrypted_text)//2 -1
+    #str1,str2 = encrypted_text[7:15:1],encrypted_text[0:7:1]
+    if encrypted_text == None or '': return(None)
+    elif n <= 0: return(encrypted_text)
+    else:
+        for i in range(n):
+            str1,str2 = encrypted_text[len(encrypted_text)//2:len(encrypted_text):1],encrypted_text[0:len(encrypted_text)//2:1]
+            text=''
+            for i in range(len(str1)):
+                #print(i)
+                if i+1 <= len(str2) != None: text = text + str1[i] + str2[i]
+                else: text = text + str1[i]
+            #print(encrypted_text[7:15:1] + encrypted_text[0:7:1])
+        return(text)
+
+def decrypt(text): # woks, only for n=1
+    str3=''
+    str1,str2 = text[len(text)//2:len(text):1],text[0:len(text)//2:1]
+    for i in range(len(str1)):
+            str3+=(str1[i]+str2[i])
+    return(str3)
+
+def decrypt(text, n):# works for n>1 but does not feel optimal
+    for i in range(n):
+        str3=''
+        str1,str2 = text[len(text)//2:len(text):1],text[0:len(text)//2:1]
+        for i in range(len(str1)):
+                str3+=(str1[i]+str2[i])
+                text=str3
+    return(text)
+
+def decrypt(text, n):# works for n>1, more optimal but still not great
+    for i in range(n):
+        str1,str2 = text[len(text)//2:len(text):1],text[0:len(text)//2:1]
+        text=''
+        for i in range(len(str1)):
+                text+=(str1[i]+str2[i])
+    return(text)
+
+    '''
+    #print("Decrypt:", args.decrypt, args.text, args.n)
+    if encrypted_text == None or '': return(None)
+    elif n <= 0: return(encrypted_text)
+    else:
+        for i in range(n):
+            text = encrypted_text[1:len(encrypted_text):2] + encrypted_text[0:len(encrypted_text):2]
+        return(text)
+    '''
 
 def encrypt(text, n):
     """encrypt function
@@ -31,9 +90,12 @@ def encrypt(text, n):
     Do this n times!
     """
     #print("Encrypt:", args.encrypt, args.text, args.n)
-    for i in range(n):
-        text = text[1:len(text):2] + text[0:len(text):2]
-    return(text)
+    if text == None or '': return(None)
+    elif n <= 0: return(text)
+    else:
+        for i in range(n):
+            text = text[1:len(text):2] + text[0:len(text):2]
+        return(text)
 
 # Module Functions and Classes
 def main(args):
@@ -46,9 +108,9 @@ def main(args):
     print("Arguments Passed:", args)
     #print(solution(*args))
     if args.decrypt:
-        decrypt(args.text, args.n)
+        print(decrypt(args.text, args.n))
     else:
-        encrypt(args.text, args.n)
+        print(encrypt(args.text, args.n))
 
 # Check to see if this file is the "__main__" script being executed
 if __name__ == '__main__':
