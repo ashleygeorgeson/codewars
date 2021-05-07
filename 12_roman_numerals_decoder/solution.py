ee8ab505f5ecfcaed92f8825c964c8635ee53929
Ashley Georgeson <ashleygeorgeson@me.com>
2021-05-07 08:36:08 +0100

    adds up but doesnt do less than e.g 4, 9 etc

--- 12_roman_numerals_decoder/solution.py
+++ 12_roman_numerals_decoder/solution.py
@@ -2,20 +2,17 @@
 
 # Docstring
 """
-Your task is to make a function which returns the sum of a sequence of integers.
+Create a function that takes a Roman numeral as its argument and returns its value as a numeric decimal integer.
 
-The sequence is defined by 3 non-negative values: begin, end, step.
-
-If begin value is greater than the end, function should returns 0
+You don't need to validate the form of the Roman numeral.
 """
 
 # imports
 import logging, os, sys
 import argparse
-#from typing import final
 
 # Module Constants
-START_MESSAGE = "Sum of a Sequence"
+START_MESSAGE = "Roman Numeral Decoder"
 
 # Module "Global" Variables
 location = os.path.abspath(__file__)
@@ -48,28 +45,37 @@ def sequence_sum(b, e, s):
     return b + sequence_sum(b + s , e, s) if b <= e else 0
 
 def solution(roman):
-    pass
+    # Convert Roman Numeral string to uppercase
+    roman = roman.upper()
+    # Count each Roman Numeral base and assign to a unique variable
+    m,d,c,l,x,v,i = roman.count('M'), roman.count('D'), roman.count('C'), roman.count('L'), roman.count('X'), roman.count('V'), roman.count(I')
+    print(m,d,c,l,x,v,i)
+    # Calculate decimal value
+    decimal = (int(m)*1000) + (int(d)*500) + (int(c)*100) + (int(l)*50) + (int(x)*10) + (int(v)*5) + (int(i)*1)
+    print(decimal)
+    return(decimal)
 
 # Module Functions and Classes
 def main(args):
-    """My main script function.
+    """Main script function.
 
     Displays the full path to this script, and a list of the arguments passed to the script.
     """
     print("="*64,'\n', START_MESSAGE, "\nScript Location:", location, "\nArguments Passed: ", args, '\n',"="*64, sep='')
-    logging.debug('Passed sequence in main: %s %s %s', args.begin, args.end, args.step)
+    logging.debug('Passed sequence in main: %s', args.roman)
     #sequence_sum(args.begin, args.end, args.step)
-    sum = sequence_sum(args.begin, args.end, args.step)
-    print(sum)
+    #sum = sequence_sum(args.begin, args.end, args.step)
+    #print(sum)
+    solution(args.roman)
 
 # Check to see if this file is the "__main__" script being executed
 if __name__ == '__main__':
     #_, *script_args = sys.argv
     parser = argparse.ArgumentParser()
-    parser = argparse.ArgumentParser(description='Returns the sum of a sequence of integers')
-    parser.add_argument('begin', type=int, help="Sequence begin")
-    parser.add_argument('end', type=int, help="Sequence end")
-    parser.add_argument('step', type=int, help="Sequence step")
+    parser = argparse.ArgumentParser(description='Decodes a Roman Numeral to Decimal integer')
+    parser.add_argument('roman', type=str, help="Roman Numeral")
+    #parser.add_argument('end', type=int, help="Sequence end")
+    #parser.add_argument('step', type=int, help="Sequence step")
     parser.add_argument("-d", "--debug", action="store_true",
                         help="enable debug logging")
     args = parser.parse_args()
